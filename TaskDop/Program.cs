5c0ee743fe12f0592ca5fd45de1d0ec804ca889f
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-07-13 21:02:56 +0300

    Решение дополнительной задачи

--- TaskDop/Program.cs
+++ TaskDop/Program.cs
@@ -6,3 +6,39 @@
 // Строка 1. Tom Marvolo Riddle
 // Строка 2. Lord Voldemort
 // Ответ: Нет - остались свободные символы.
+
+using System;
+using static System.Console;
+using System.Linq;
+Clear();
+
+string TextOne = "Tom Marvolo Riddle";
+string TextTwo = "I am Lord Voldemort";
+string TextThree = "Lord Voldemort";
+
+WriteLine($"Строка 1 - {TextOne}");
+WriteLine($"Строка 2 - {TextTwo}");
+StringСomparison(TextOne, TextTwo);
+WriteLine();
+WriteLine($"Строка 1 - {TextOne}");
+WriteLine($"Строка 2 - {TextThree}");
+StringСomparison(TextOne, TextThree);
+
+void StringСomparison(string firstText, string secondText)
+{
+   // Перобразуем символы строк
+   string newFirstText = String.Concat(firstText.ToLower().Replace(" ", "").OrderBy(ch => ch));
+   string newSecondText = String.Concat(secondText.ToLower().Replace(" ", "").OrderBy(ch => ch));
+
+   if (newFirstText.Length == newSecondText.Length)
+   {
+      int count = 0;
+      for (int i = 0; i < newFirstText.Length; i++)
+      {
+         if (newFirstText[i] == newSecondText[i]) count++;
+      }
+      if (count == newFirstText.Length) WriteLine("Да можно из символов первой строки, собрать вторую");
+      else WriteLine("Нет - остались свободные символы.");
+   }
+   else WriteLine("Нет - остались свободные символы.");
+}
